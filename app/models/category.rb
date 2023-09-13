d290e1625dd4631f5b92c8823ded9f08b5c9318c
yosaddis <email.to.yoseph@gmail.com>
2023-09-13 19:58:30 +0300

    Add validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,17 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :category_transaction
+  has_many :transaction, through: :category_transaction
+
+  validates_presence_of :user
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_amount
+    total = 0
+    transaction.each do |transaction|
+      total += transaction.amount
+    end
+    total
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :category
+  has_many :transaction, foreign_key: :user_id
+  
+  validates :name, presence: true
 end
