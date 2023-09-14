47cd2e71a7c8166b52bb58adb7e7e8b5e1d62f65
yosaddis <email.to.yoseph@gmail.com>
2023-09-14 23:11:34 +0300

    Fixed linter messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :category
-  has_many :transaction, foreign_key: :user_id
+  # has_many :category
+  # has_many :transaction, foreign_key: :author_id
 
   validates :name, presence: true
 end
