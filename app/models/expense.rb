6b67b1c018b859ba7c35731a080966e9c46955b2
yosaddis <email.to.yoseph@gmail.com>
2023-09-15 17:20:15 +0300

    Update the transaction model to expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
-class Transaction < ApplicationRecord
+class Expense < ApplicationRecord
   belongs_to :user
-  has_many :category_transaction, depedent: :destroy
-  has_many :category, through: :category_transaction
+  has_many :category_expense, dependent: :destroy
+  has_many :category, through: :category_expense
 
   validates_presence_of :name, :amount
   validates :amount, presence: true, numericality: { greater_than: 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -23,22 +23,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_164600) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "category_transactions", force: :cascade do |t|
+  create_table "category_expenses", force: :cascade do |t|
     t.bigint "category_id", null: false
-    t.bigint "transaction_id", null: false
+    t.bigint "expense_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_category_transactions_on_category_id"
-    t.index ["transaction_id"], name: "index_category_transactions_on_transaction_id"
+    t.index ["category_id"], name: "index_category_expenses_on_category_id"
+    t.index ["expense_id"], name: "index_category_expenses_on_expense_id"
   end
 
-  create_table "transactions", force: :cascade do |t|
+  create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.float "amount"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_transactions_on_user_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_164600) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "category_transactions", "categories"
-  add_foreign_key "category_transactions", "transactions"
-  add_foreign_key "transactions", "users"
+  add_foreign_key "category_expenses", "categories"
+  add_foreign_key "category_expenses", "expenses"
+  add_foreign_key "expenses", "users"
 end
